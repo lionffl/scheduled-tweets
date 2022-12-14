0a6c9a8df1c0363a23784ded4bdea2db6b3409d7
Felipe <lionffl@gmail.com>
2022-12-14 15:53:02 -0300

    handling sign up errors

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "Must be a valid email" }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   root to: "main#index"
+
   get "about", to: "about#index"
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
 end

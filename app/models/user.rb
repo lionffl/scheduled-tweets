39b2b4a876b1d3cf9474324ed5199bdb5b0db706
Felipe <lionffl@gmail.com>
2022-12-16 09:22:10 -0300

    user/twitter_account association added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :twitter_accounts
+
   has_secure_password
+  
   validates :email, uniqueness: true, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "Must be a valid email" }
 end

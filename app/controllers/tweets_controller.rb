82bbc8ff2143a29671926d798280c95766f7bdf9
Felipe <lionffl@gmail.com>
2022-12-16 15:40:58 -0300

    refactor/added: snippet for flash error / schedule tweet page added

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,4 +4,22 @@ class TweetsController < ApplicationController
   def index
     @tweets = Current.user.tweets
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Current.user.tweets.create(tweet_params)
+
+    if @tweet.save
+      redirect_to @tweet, notice: "Tweet was scheduled"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def tweet_params
+    params.required(:tweet).permit(:twitter_account_id, :body, :publish_at)
+  end
 end

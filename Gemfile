13cdf16db9a02f0655d08d035a9552a5d492e8bb
Felipe <lionffl@gmail.com>
2022-12-17 08:45:56 -0300

    added sidekiq to handle jobs

--- Gemfile
+++ Gemfile
@@ -75,3 +75,5 @@ gem "omniauth-twitter", "~> 1.4"
 gem "omniauth-rails_csrf_protection", "~> 1.0"
 
 gem "twitter", "~> 7.0"
+
+gem "sidekiq", "~> 7.0"
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Handle job to sidekiq
+  config.active_job.queue_adapter = :sidekiq
 end

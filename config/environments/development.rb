e83cff2c1e01113c4c76f2ebf336fa3dfda510af
Felipe <lionffl@gmail.com>
2022-12-21 18:38:29 -0300

    fix: credentials sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,7 +73,7 @@ end
 
 ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey', # This is the string literal 'apikey', NOT the ID of your API key
-  :password => Rails.application.credentials.dig(:twitter, :api_secret), # This is the secret sendgrid API key which was issued during API key creation
+  :password => Rails.application.credentials.dig(:sendgrid, :api_secret), # This is the secret sendgrid API key which was issued during API key creation
   :domain => 'tweet-schedule.geografialinks.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
